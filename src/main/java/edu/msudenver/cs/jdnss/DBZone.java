e0327234e1e47146f880389035c90fbad4399b44
Ge Sha At u18vmHP8570w <gesha@uniposta.de>
2020-03-21 16:21:12 +0100

    FIX: FiXmE--

--- src/main/java/edu/msudenver/cs/jdnss/DBZone.java
+++ src/main/java/edu/msudenver/cs/jdnss/DBZone.java
@@ -5,10 +5,13 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.message.ObjectMessage;
 
 import java.util.ArrayList;
+import java.util.List;
 
 class DBZone extends Zone {
     private static final Logger logger = JDNSS.logger;
     private final String zoneName;
+	private int domainId;
+	private DBConnection dbConnection;
 
     DBZone() { zoneName = null; }
 
@@ -20,18 +23,18 @@ class DBZone extends Zone {
     DBZone(final String zoneName, final int domainId, final DBConnection dbConnection)
     {
         this.zoneName = zoneName;
-        int domainId1 = domainId;
-        DBConnection dbConnection1 = dbConnection;
+        this.domainId = domainId;
+        this.dbConnection = dbConnection;
     }
 
     public String getName() { return zoneName; }
 
-    public ArrayList<RR> get(final RRCode type, final String name)
+    public List<RR> get(final RRCode type, final String name)
     {
         logger.traceEntry(new ObjectMessage(type));
         logger.traceEntry(new ObjectMessage(name));
 
-        // FIXME return dbConnection.get(type, name, domainId);
-        return null;
+        return dbConnection.get(type, name, domainId);
+        
     }
 }
